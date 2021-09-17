d49a289d0af3258d4bd9a7aec8a33b733f71ffd6
Zhan Lun Ng <zlun0817@gmail.com>
2021-09-17 22:20:45 +0800

    init, basic piano layout

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import Piano from "./components/Piano";
+
+function App() {
+  return (
+    <div>
+      <Piano />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Piano.js
@@ -0,0 +1,35 @@
+import React from 'react'
+
+export default function Piano() {
+  return (
+    <div className="piano">
+      <div className="piano-keybed">
+        <div className="piano-key piano-key-white">
+          <span>C</span>
+        </div>
+        <div className="piano-key piano-key-black">
+          <span>C#</span>
+        </div>
+        <div className="piano-key piano-key-white">
+          <span>D</span>
+        </div>
+        <div className="piano-key piano-key-black">
+          <span>D#</span>
+        </div>
+        <div className="piano-key piano-key-white">
+          <span>E</span>
+        </div>
+        <div className="piano-key piano-key-white">
+          <span>F</span>
+        </div>
+        <div className="piano-key piano-key-black">
+          <span>F#</span>
+        </div>
+        <div className="piano-key piano-key-white">
+          <span>G</span>
+        </div>
+      </div>
+
+    </div>
+  )
+}
