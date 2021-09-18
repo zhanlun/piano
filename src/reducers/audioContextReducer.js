d6124c996ba83e49f9ff77f4482ce1731aaf6930
Zhan Lun Ng <zlun0817@gmail.com>
2021-09-18 11:49:56 +0800

    add timeout stopping note

--- src/reducers/audioContextReducer.js
+++ src/reducers/audioContextReducer.js
@@ -8,20 +8,27 @@ let sustainLevel = 0.8;
 let releaseTime = 0.3;
 let noteLength = 1;
 
-function testPlay(pitch) {
+function startNote(pitch) {
   const oscillator = context.createOscillator();
   const noteGain = context.createGain();
   noteGain.gain.setValueAtTime(0, 0);
   noteGain.gain.linearRampToValueAtTime(sustainLevel, context.currentTime + noteLength * attackTime);
   noteGain.gain.setValueAtTime(sustainLevel, context.currentTime + noteLength - noteLength * releaseTime);
-  noteGain.gain.linearRampToValueAtTime(0, context.currentTime + noteLength);
+  noteGain.gain.linearRampToValueAtTime(0, context.currentTime + 10);
 
   oscillator.type = 'sine';
   oscillator.frequency.setValueAtTime(pitch, 0);
   oscillator.start(0);
-  oscillator.stop(context.currentTime + noteLength);
   oscillator.connect(noteGain);
   noteGain.connect(masterVolume);
+  return { oscillator, noteGain }
+}
+
+function stopNote(oscillator, noteGain) {
+  noteGain.gain.setValueAtTime(noteGain.gain.value, context.currentTime);
+  noteGain.gain.exponentialRampToValueAtTime(0.0001, context.currentTime + 0.03);
+  oscillator.stop(context.currentTime + 5);
+  // then oscillator auto delete by garbage collector
 }
 
 const initialState = {
@@ -35,7 +42,10 @@ const initialState = {
 const audioContextReducer = (state = initialState, action) => {
   switch (action.type) {
     case 'audioContext/play':
-      testPlay(action.pitch)
+      const { oscillator, noteGain } = startNote(action.pitch)
+      setTimeout(() => {
+        stopNote(oscillator, noteGain)
+      }, 3000);
       return state
     default:
       return state
